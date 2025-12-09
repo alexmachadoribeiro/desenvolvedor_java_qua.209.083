ae3e78272d705a5f4e8a5b1c92577cef9958c77c
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-12-08 21:35:14 -0300

    Aula do dia 08/12/2025.

---
+++ 04_front_end/12_calculadora/js/codigo.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+
+const calcular = () => {
+    // declaração de variáveis locais
+    let x = document.querySelector('#x').value;
+    let y = document.querySelector('#y').value;
+    let operador = document.querySelector('#operador').value;
+    let result = 0;
+
+    // verifica a operação selecionada
+    switch (operador) {
+        case 'soma':
+            result = x+y;
+            document.querySelector('#result').innerHTML = result;
+            break;
+        // TODO: terminar switch
+    }
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    calcular();
+});
