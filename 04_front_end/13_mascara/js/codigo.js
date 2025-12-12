52afd8082104cfff0178210244ef7e2ab51e079d
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-12-12 20:07:40 -0300

    Aula do dia 12/12/2025.

--- 04_front_end/13_mascara/js/codigo.js
+++ 04_front_end/13_mascara/js/codigo.js
@@ -1,4 +1,7 @@
 const form = document.querySelector('form');
+const cpfMask = document.querySelector('#cpf');
+const telefoneMask = document.querySelector('#telefone');
+const cepMask = document.querySelector('#cep');
 
 const exibirInfo = () => {
     // declaração de variáveis locais
@@ -23,7 +26,28 @@ const exibirInfo = () => {
     form.reset();
 }
 
-// TODO: criar funções de máscara de entrada para cep, cpf, telefone e data
+cpfMask.addEventListener('input', function() {
+    this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+});
+
+telefoneMask.addEventListener('input', function() {
+    this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4,5})(\d)/, '$1-$2')
+        .replace(/(-\d{4})\d+?$/, '$1');
+});
+
+cepMask.addEventListener('input', function() {
+    this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1');
+});
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
